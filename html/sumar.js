f47553853a012f0c6ef0f60f080012ff91d7a396
mlautaroj <mlautaroj@gmail.com>
2023-02-08 23:46:21 -0300

    primer repositorio

---
+++ html/sumar.js
@@ -0,0 +1,23 @@
+
+
+console.log("Hola");
+
+
+document.getElementById('sumar').addEventListener('click', function(){
+    let numeroA = document.getElementById('numero1').value;
+    console.log('el valor del numero A es: '+numeroA);
+
+    let numeroB = document.getElementById('numero2').value;
+    console.log('el valor del numero B es: '+numeroB);
+
+    let resultado = sumar(parseInt(numeroA), parseInt(numeroB));
+    console.log('el resultado es: '+resultado);
+
+    document.getElementById('resultado').innerHTML = resultado
+    document.getElementById('contenedor_resultado').style.display = "block";
+    
+})
+
+function sumar(a,b) {
+    return a + b;
+}
